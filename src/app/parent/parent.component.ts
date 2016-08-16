2cc6c1bc627b2935411c56361f3f759334c378f2
Daniel Jenkins <DanielPatrickJenkins@gmail.com>
2016-08-16 16:55:12 -0700

    Updating parent component

---
+++ src/app/parent/parent.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'practice-parent',
+  templateUrl: 'parent.component.html',
+  styleUrls: ['parent.component.css']
+})
+export class ParentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
